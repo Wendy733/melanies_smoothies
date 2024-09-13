1094d2c632c68e453b471c74b5d9f4850a43af6a
Wendy733 <wbeltran@santander.com.mx>
2024-09-13 17:37:52 -0600

    Update streamlit_app.py
    se agrega el marco de datos fv_df para Fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,6 @@ session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
-
 ingredients_list=st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
@@ -34,6 +31,11 @@ if ingredients_list:
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+ name_on_order +"""') """
+
+# New section to display fruityvice nutrition information
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 #st.stop()
 #st.write(my_insert_stmt)
