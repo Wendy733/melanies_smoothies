dfac849b87cb7a25d1b77d6cc2bda4d0576d9612
Wendy733 <wbeltran@santander.com.mx>
2024-09-13 16:25:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ session = cnx.session()
 name_on_order = st.text_input("Name on Smoothies:")
 st.write("The name on your Smoothie will be: ", name_on_order)
 
-#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 ingredients_list=st.multiselect(
     'Choose up to 5 ingredients:'
