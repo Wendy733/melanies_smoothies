c2d5f7a2f60a763b989a2fd7a74ba88ac6e568a4
Wendy733 <wbeltran@santander.com.mx>
2024-09-13 17:54:02 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,15 +23,14 @@ ingredients_list=st.multiselect(
     , my_dataframe
     ,max_selections=6)
 
-import requests
 if ingredients_list:
-    ingredients_string=''
+        ingredients_string=''
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ''
-        st.subheader(fruit_choosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        for fruit_chosen in ingredients_list:
+                ingredients_string += fruit_chosen + ' '
+                #st.subheader(fruit_choosen + ' Nutrition Information')
+                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+                fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+ name_on_order +"""') """
